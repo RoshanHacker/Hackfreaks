ac196c51902dd5f28d06fa56283f5c605e667f5d
NandyDark <58636468+nandydark@users.noreply.github.com>
2021-02-24 06:47:20 -0500

    Update hackfreaks.py

--- Hackfreaks/hackfreaks.py
+++ Hackfreaks/hackfreaks.py
@@ -1,5 +1,5 @@
 from telethon import events
-from Hackfreaks import telethn
+from Hackfreaksrobot import telethn
 
 """Triggers start command in pm and in groupchats"""
 def Indianofficialrobot(**args):
