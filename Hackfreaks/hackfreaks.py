e102ad53f07574c01c9059ad3f2bc0a3e559e0b0
RoshanHacker <74089939+RoshanHacker@users.noreply.github.com>
2021-02-24 18:05:31 +0530

    Update hackfreaks.py

--- Hackfreaks/hackfreaks.py
+++ Hackfreaks/hackfreaks.py
@@ -2,7 +2,7 @@ from telethon import events
 from hackfreaksrobot import telethn
 
 """Triggers start command in pm and in groupchats"""
-def Indianofficialrobot(**args):
+def hackfreaksrobot(**args):
     """New message."""
     pattern = args.get('pattern', None)
     r_pattern = r'^[/!]'
