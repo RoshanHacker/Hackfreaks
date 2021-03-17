670a71de7ae0f4bf09bf9397e20315565f8b9a38
RoshanHacker <74089939+RoshanHacker@users.noreply.github.com>
2021-03-17 21:26:45 +0530

    Update userinfo.py

--- Hackfreaks/modules/userinfo.py
+++ Hackfreaks/modules/userinfo.py
@@ -516,7 +516,7 @@ Examples:
  • `/info`*:* get information about a user. 
  
 *What is that health thingy?*
- Come and see [HP System explained](https://t.me/HackfreaksUpdates/8)
+ Come and see [HP System explained](https://t.me/indianbotupdate/4)
 """
 
 SET_BIO_HANDLER = DisableAbleCommandHandler("setbio", set_about_bio)
